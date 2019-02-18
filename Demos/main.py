460c9983140f7a3f72dfa84a9c65fb5948dff561
Zachary Torricelli <35783419+zacharytorricelli@users.noreply.github.com>
2019-02-18 09:35:38 -0600

    Add files via upload

---
+++ Demos/main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+search = input("Enter search term:")
+params = {"q" : search}
+r = requests.get("http://www.bing.com/search", params=params)
+
+soup = BeautifulSoup(r.text)
+print(soup.prettify())
+
