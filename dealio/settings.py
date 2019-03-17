3321113437093ddc29bd09557499e3f981ce1c97
Jason <michaeljpearson1980@gmail.com>
2019-03-16 22:27:53 -0500

    Updates settings.py to alter the static_root

--- dealio/settings.py
+++ dealio/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'deals/../deals/../static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'deals/../static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
