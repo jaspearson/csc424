153604d893ad13f7d7ada7861f8f123d6ea772cd
Jason <michaeljpearson1980@gmail.com>
2019-03-17 22:46:22 -0500

    Updates settings.py to add STATICFILES_DIRS variable. Adds the search-icon.png Updates the base.html template to add the search-icon.png to the search button.

--- dealio/settings.py
+++ dealio/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'deals/../static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+
+]
