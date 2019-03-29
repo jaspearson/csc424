b5659629dabaa2ddc8cc56359bb457a598e79c9c
Jason <michaeljpearson1980@gmail.com>
2019-03-28 19:22:33 -0500

    Updates to walmart scraper. Adds store id, product id

--- Classes/walmartScraper.py
+++ Classes/walmartScraper.py
@@ -1,9 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+from deals.models import Deal
+from decimal import *
 
 class Walmart():
 
+    store_id = 1
+
     def __init__(self):
+        self.store_id = 1
         print("The wal-mart class was initialized.")
 
     def get_deals(self):
@@ -72,22 +77,63 @@ class Walmart():
             title = product.find("a", class_="product-title-link")
             price = product.find("span", class_="price-group")
             url = product.find("a", class_="product-title-link")
+            actual_url = "http://walmart.com" + url.get("href")
             image_container = product.find("div", class_="search-result-productimage gridview")
             image_url = image_container.find("img", src=True)['src']
             store_icon = "images/walmart-icon.png"
 
+
+
             if title != None and price != None and url != None:
 
-                print("%s - %s" % (title.text, price.text))
+                # Get the product ID of the item from the URL.
+                # The product ID is usually encoded into the URL to make it unique.
+                # We will use this value to keep things unique in our database.
+                # In Wal-mart's case the product ID was at the end of the URL.
+                # Other stores might be in the middle of the URL and a different
+                # method for stripping it out may be needed.
+
+                # Use the / character as the delimiter to split the url in to different pieces and store it in an
+                # array.
+                split_url = actual_url.split('/')
+
+                # Get the number of elements in the splitURL array.
+                array_size = len(split_url)
+
+                # Get the position of the product ID.
+                # In the case of Wal-mart, it is stores the item id at the end of the URL.
+                # Therefore, it will be the last element in the array when the URL is split up.
+                product_id_element = array_size-1
+
+                # Get the product_id
+                product_id = split_url[product_id_element]
+
+                # dealio_id will be the store_id - product_id
+                # Example: 1-19758129
+                dealio_id = str(self.store_id) + "-" + product_id
+
+                print("%s - %s, %s" % (title.text, price.text.strip(' - $'), dealio_id))
 
                 record = {
-                    'title': title.text,
+                    'title': title.text.strip('...'),
                     'price': price.text,
-                    'url': "http://walmart.com" + url.get("href"),
+                    'url': actual_url,
                     'image_url' : image_url,
-                    'store_icon': store_icon
+                    'store_icon': store_icon,
+                    'product_id': dealio_id
                 }
 
+                # jaspearson's attempt to save records to the mysql database.
+                # Will continue work on this part later. Commenting out for now.
+                """r = Deal(url="http://walmart.com" + url.get("href"),
+                         image_url=image_url, title=title.text,
+                         price=Decimal(price.text.strip(' - $')),
+                         featured_product=False,
+                         store_id=self.store_id,
+                         product_id=dealio_id)
+
+                r.save()
+                """
                 # Append each new product and attributes to the dictionary object
                 extracted_records.append(record)
 
