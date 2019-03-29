c7fd14bf2c0656020eb5903e3bc2e275c0e1923d
Jason <michaeljpearson1980@gmail.com>
2019-03-29 18:49:22 -0500

    Adds staples icon. Removes redundant get_deals method from walmart scrapper.

--- Classes/walmartScraper.py
+++ Classes/walmartScraper.py
@@ -11,46 +11,6 @@ class Walmart():
         self.store_id = 1
         print("The wal-mart class was initialized.")
 
-    def get_deals(self):
-
-        url = 'https://www.walmart.com/browse/electronics/3944/?cat_id=3944&facet=special_offers%3AClearance%7C%7Cspecial_offers%3ARollback%7C%7Cspecial_offers%3ASpecial+Buy'
-
-        # Make the request
-        the_request = requests.get(url)
-
-        # Parse the request
-        soup = BeautifulSoup(the_request.text, 'html.parser')
-
-        # Get the table that holds the products
-        products_table = soup.find("div", attrs={'id': 'searchProductResult'})
-
-        # Get all product container divs
-        products = products_table.find_all("div", class_="search-result-gridview-item-wrapper arrange")
-
-        # Initialize a dictionary object
-        extracted_records = []
-
-        # Loop through the the html products and extract key pieces of information.
-        # title of product, price of the product, url to the product, etc
-        for product in products:
-
-            title = product.find("a", class_="product-title-link").text
-            price = product.find("span", class_="price-group").text
-            url = "http://walmart.com" + product.find("a", class_="product-title-link").get('href')
-
-            print("%s - %s" % (title, price))
-            record = {
-                'title': title,
-                'price': price,
-                'url': url
-            }
-
-            # Append each new product and attributes to the dictionary object
-            extracted_records.append(record)
-
-        # Return the extracted records to the calling program
-        return extracted_records
-
     def get_deals(self, this_keyword):
 
         url = "https://www.walmart.com/search/?query="
