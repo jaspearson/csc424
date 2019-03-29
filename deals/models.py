74b01aaecca7951102a54d6546f1210520a7c53e
Jason <michaeljpearson1980@gmail.com>
2019-03-28 19:34:23 -0500

    Adds the Deal model to the deals app.

--- deals/models.py
+++ deals/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.urls import reverse
-from Classes.walmartScraper import Walmart
+#from Classes.walmartScraper import Walmart
 
 
 # Create your models here.
-class Deals():
+class Deals:
 	title = "title"
 	price = "price"
 	url = "url"
@@ -20,8 +20,21 @@ class Deals():
 		self.url = url
 
 
-def get_absolute_url(self):
-	return reverse('blog.views.deal', args=[])
+class Deal(models.Model):
+	url = models.CharField(max_length=1000)
+	image_url = models.CharField(max_length=1000)
+	title = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=10, decimal_places=2)
+	featured_product = models.BooleanField(default=False)
+	store_id = models.IntegerField()
+	product_id = models.CharField(max_length=50, unique=True)
 
+	def get_absolute_url(self):
+		return reverse('deals.views.deal', args=[])
 
-my_test = Walmart().get_deals('TV')
+	class Meta:
+
+		def __unicode__(self):
+			return u'%s' % self.title
+
+# my_test = Walmart().get_deals('TV')
